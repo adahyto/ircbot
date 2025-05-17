4324dba549f0f2d69fd15f3ea4a6d57fb4607826
Alex <adahy.to@gmail.com>
2025-05-17 09:04:44 +0200

    refactor

--- index.js
+++ index.js
@@ -1,3 +1,89 @@
+// const WebSocket = require("ws");
+
+class IrcSocket {
+  constructor(url) {
+    this.connection = this.newConnection(url);
+  }
+
+  newConnection(url) {
+    return new WebSocket(url);
+  }
+
+  keepConnection() {
+    setInterval(() => {
+      this.connection.send("");
+    }, 45000);
+  }
+
+  setNick(nick) {
+    this.connection.send(`NICK ${nick}`);
+  }
+
+  joinChanel(name) {
+    this.connection.send(`JOIN ${name}`);
+    this.keepConnection();
+  }
+
+  quit(name) {
+    this.connection.send(`QUIT ${name}`);
+  }
+
+  send(msg) {
+    this.connection.send(msg);
+  }
+
+  onMsg(func) {
+    this.connection.onmessage = (event) => {
+      func(event);
+    };
+  }
+  onOpen(func) {
+    this.connection.onopen = (event) => {
+      func(event);
+    };
+  }
+}
+
+class IrcFickerBot {
+  irc = new IrcSocket("wss://web.libera.chat/webirc/websocket/");
+  constructor(chanel, fickConditions, fickTopics, nick) {
+    this.chanel = chanel;
+    this.fickConditions = fickConditions;
+    this.fickTopics = fickTopics;
+    this.nick = nick;
+  }
+
+  init() {
+    this.irc.onOpen((event) => {
+      this.irc.setNick(this.nick);
+      this.irc.joinChanel(this.chanel);
+
+      console.log("connection open: ", event);
+    });
+
+    this.irc.onMsg((event) => {
+      if (this.isFickNeeded(event.data)) {
+        this.irc.send(this.fick());
+      }
+    });
+  }
+
+  stop() {
+    this.irc.quit();
+  }
+
+  isFickNeeded(msg) {
+    if (this.fickConditions(msg)) {
+      return true;
+    }
+  }
+
+  fick() {
+    const ran = Math.floor(Math.random() * this.fickTopics.length + 1);
+    return `*** jebiemy ${this.fickTopics[ran]} ***`;
+  }
+}
+
 let multiFickBot = null;
 
 const runFickBot = () => {
