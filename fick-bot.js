68aa3d36ef68d1e84f56690202f09cddab38a106
Alex <adahy.to@gmail.com>
2025-05-17 09:16:22 +0200

    refactor

--- fick-bot.js
+++ fick-bot.js
@@ -1,3 +1,50 @@
+// const WebSocket = require("ws");
+
+class IrcSocket {
+  constructor(url) {
+    this.connection = this.newConnection(url);
+  }
+
+  newConnection(url) {
+    return new WebSocket(url);
+  }
+
+  keepConnection() {
+    setInterval(() => {
+      this.connection.send("");
+    }, 45000);
+  }
+
+  setNick(nick) {
+    this.connection.send(`NICK ${nick}`);
+  }
+
+  joinChanel(name) {
+    this.connection.send(`JOIN ${name}`);
+    this.keepConnection();
+  }
+
+  quit(name) {
+    this.connection.send(`QUIT ${name}`);
+  }
+
+  send(msg) {
+    this.connection.send(msg);
+  }
+
+  onMsg(func) {
+    this.connection.onmessage = (event) => {
+      func(event);
+    };
+  }
+
+  onOpen(func) {
+    this.connection.onopen = (event) => {
+      func(event);
+    };
+  }
+}
+
 class IrcFickerBot {
   irc = new IrcSocket("wss://web.libera.chat/webirc/websocket/");
   constructor(chanel, fickConditions, fickTopics, nick) {
