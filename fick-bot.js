86f48bde2408bd008f575ca426c0a6011b93bd36
Alex <adahy.to@gmail.com>
2025-05-17 10:50:00 +0200

    refactor

--- fick-bot.js
+++ fick-bot.js
@@ -8,16 +8,16 @@ class IrcFickerBot {
   }
 
   init() {
-    this.irc.client().onOpen((event) => {
+    this.irc.client.onOpen((event) => {
       this.irc.setNick(this.nick);
       this.irc.joinChanel(this.chanel);
     });
 
-    this.irc.client().onMsg((event) => {
-      if (this.isFickNeeded(event.data)) {
-        this.irc.send(this.fick());
-      }
-    });
+    // this.irc.client.onMsg(event) => {
+    //   if (this.isFickNeeded(event.data)) {
+    //     this.irc.send(this.fick());
+    //   }
+    // });
   }
 
   stop() {
