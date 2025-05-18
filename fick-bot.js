91a95965052c8fae6d7e9ca0f460fa317b50d434
Alex <adahy.to@gmail.com>
2025-05-18 17:41:22 +0200

    refactor

--- fick-bot.js
+++ fick-bot.js
@@ -18,6 +18,7 @@ class FickerIrcBot extends IrcBot {
       this.irc.setNick(this.nick);
       this.irc.joinChanel(this.chanel);
     });
+
     this.irc.connection.addEventListener("message", async (e) => {
       var line = e.data;
       if (line instanceof Blob) {
